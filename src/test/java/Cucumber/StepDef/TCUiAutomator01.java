d8b4a1499fdf8e5be53364e8c1c3aad639154cd5
Fatih YILMAZ <fatih.yilmaz@foreks.com>
2021-05-20 20:24:19 +0300

    Cases_2

---
+++ src/test/java/Cucumber/StepDef/TCUiAutomator01.java
@@ -0,0 +1,9 @@
+package Cucumber.StepDef;
+/*
+ApıDemos uygulamsını açınız
+Uygulamanın açıldığını verify ediniz
+Views e tıkla
+Kaç tane clicklenebilir veri olduğunun sayısının konsola yazdır
+ */
+public class TCUiAutomator01 {
+}
